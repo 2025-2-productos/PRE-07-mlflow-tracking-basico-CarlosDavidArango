93eff2293dbd833135709994d2fb2661283696d9
CarlosDavidArango <92811129+CarlosDavidArango@users.noreply.github.com>
2025-12-06 12:06:26 -0500

    Update make_predictions.py

--- make_predictions.py
+++ make_predictions.py
@@ -1,6 +1,9 @@
 import mlflow
 import pandas as pd
 
+# Configura el tracking URI para que coincida con el de entrenamiento
+mlflow.set_tracking_uri("sqlite:///mlruns.db")
+
 FILE_PATH = "data/winequality-red.csv"
 
 df = pd.read_csv(FILE_PATH)
@@ -8,7 +11,8 @@ df = pd.read_csv(FILE_PATH)
 y = df["quality"]
 X = df.drop(columns=["quality"])
 
+# Carga el modelo usando el run_id y el artifact_path
 logged_model = "runs:/23130ed670f648a0990d32f5b0c77507/model"
-logged_model = mlflow.pyfunc.load_model(logged_model)
-predictions = logged_model.predict(X)
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+predictions = loaded_model.predict(X)
 print(predictions)
